93cb878ba8862b0ce7c80f2bc1d687b18cfbc268
TylerChen1200 <tychen2003@gmail.com>
2020-03-29 00:43:05 -0700

    Results Page
    User Inputs anything as there is no dummy data, prints randomly generated "positive" or "negative" upon Tim's request.

--- berify-react/pages/ResultPage.js
+++ berify-react/pages/ResultPage.js
@@ -1,38 +1,116 @@
 import React, { Component } from 'react';
+
 import { connect } from 'react-redux';
+
 import ChatBox from '../components/ChatBox';
+
 import axios from 'axios';
-import { Tabs, Radio, Button, Select, Input, Modal } from 'antd';
-import * as types from '../redux/types';
+
 import { Router } from '../routes';
 
-class TrackerPage extends Component {
-    static async getInitialProps({ store }) {
-        store.dispatch({ type: types.CHANGE_PAGE, payload: 'r' });
-        console.log('getInitialProps');
-    }
+import { Tabs, Radio, Button, Select, Input, Modal } from 'antd';
 
-    state = {
-        text: ''
-    };
+import * as types from '../redux/types';
+
+ 
+
+class TrackerPage extends React.Component {
+
+    static async getInitialProps({ store }) {
+
+        store.dispatch({ type: types.CHANGE_PAGE, payload: 'r' });
+
+        console.log('getInitialProps');
+
+      }
+
+      constructor()
+
+      {
+
+          super();
+
+          this.state = {text:null};
+
+      }
+
+ 
+
+    positiveOrNegative=()=>{
+
+     var posOrNeg = Math.random()*(1-0)+0;
+
+       if(posOrNeg <0.5)
+
+       {
+
+        this.setState({text:"Positive"});
+
+        }
+
+        else
+
+      {
+
+          this.setState({text:"Negative"});
+
+     }
+
+     }
+
+     
 
     render() {
-        return (
-            <div style={{ width: '90vw', margin: '3vw' }}>
-                <h2>Results Page (Tyler)</h2>
+
+       return (
+
+           
+
+            <div style={{ width: '90vw', margin: '3vw', alignContent: 'center'}}>
+
+                <h2>Please Enter Your Code For Your Results</h2>
+
                 <h2>
-                    {' '}
-                    Need to be able to enter a code, press a button, and show whether the test
-                    result is positive or negative, or that the test hasn't been tested yet{' '}
+
+                    <body>
+
+                        <Input id ="text1"></Input>
+
+                        <Button type="primary"
+
+                        onClick={this.positiveOrNegative}>
+
+                            Submit
+
+                        </Button>
+
+                        <label>{this.state.text}</label>
+
+                    </body>
+
                 </h2>
+
             </div>
-        );
+
+            
+
+        )
+
     }
+
 }
 
+ 
+
 const mapStateToProps = state => {
+
     const { user, isAuthenticated, error } = state.auth;
+
     return { user, isAuthenticated, error };
+
 };
 
+ 
+
 export default connect(mapStateToProps, null)(TrackerPage);
+
