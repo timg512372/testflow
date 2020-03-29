67ed877e5f9792cbccd335b0b0fd7bd3bfa2e73f
William <51173375+William-Cen@users.noreply.github.com>
2020-03-28 19:01:57 -0700

    Update index.js

--- berify-react/pages/index.js
+++ berify-react/pages/index.js
@@ -70,7 +70,7 @@ class HomePage extends Component {
                         }}>
                          TestFlow tracks test inventory on the blockchain, securing your testing results
                          </h1>
-                         <img style= {{}} alt="numberOne" src="/static/numberOne.png" />
+                         <img  alt="numberOne" src="/static/numberOne.png" />
                          <h2>Everytime your testing kit is moved, its QR code will be scanned</h2>
                     </div>
                 </div>
