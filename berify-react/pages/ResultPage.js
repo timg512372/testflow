b192a0e6b28151ac8300264d5e25a957c6096a79
Tim <timg51237@gmail.com>
2020-03-29 10:25:48 -0700

    Cleanup

--- berify-react/pages/ResultPage.js
+++ berify-react/pages/ResultPage.js
@@ -12,105 +12,68 @@ import { Tabs, Radio, Button, Select, Input, Modal } from 'antd';
 
 import * as types from '../redux/types';
 
- 
-
-class TrackerPage extends React.Component {
-
+class ResultPage extends React.Component {
     static async getInitialProps({ store }) {
-
         store.dispatch({ type: types.CHANGE_PAGE, payload: 'r' });
-
-        console.log('getInitialProps');
-
-      }
-
-      constructor()
-
-      {
-
-          super();
-
-          this.state = {text:null};
-
-      }
-
- 
-
-    positiveOrNegative=()=>{
-
-     var posOrNeg = Math.random()*(1-0)+0;
-
-       if(posOrNeg <0.5)
-
-       {
-
-        this.setState({text:"Positive"});
-
-        }
-
-        else
-
-      {
-
-          this.setState({text:"Negative"});
-
-     }
-
-     }
-
-     
-
-    render() {
-
-       return (
-
-           
-
-            <div style={{ width: '90vw', margin: '3vw', alignContent: 'center'}}>
-
-                <h2>Please Enter Your Code For Your Results</h2>
-
-                <h2>
-
-                    <body>
-
-                        <Input id ="text1"></Input>
-
-                        <Button type="primary"
-
-                        onClick={this.positiveOrNegative}>
-
-                            Submit
-
-                        </Button>
-
-                        <label>{this.state.text}</label>
-
-                    </body>
-
-                </h2>
-
-            </div>
-
-            
-
-        )
-
     }
 
+    constructor() {
+        super();
+        this.state = { text: '', id: '', error: '' };
+    }
+
+    positiveOrNegative = () => {
+        var posOrNeg = Math.random() * (1 - 0) + 0;
+
+        if (posOrNeg < 0.5) {
+            this.setState({ text: 'Positive' });
+        } else {
+            this.setState({ text: 'Negative' });
+        }
+    };
+
+    render() {
+        return (
+            <div
+                style={{
+                    width: '94vw',
+                    margin: '3vw',
+                    alignItems: 'center',
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}
+            >
+                <h1 style={{ textAlign: 'center' }}>Please Enter Your Code For Your Results</h1>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center'
+                    }}
+                >
+                    <Input
+                        id="text1"
+                        onChange={event => this.setState({ id: event.target.value })}
+                        style={{ width: '70vw' }}
+                    ></Input>
+                    <Button
+                        type="primary"
+                        onClick={this.positiveOrNegative}
+                        style={{ marginLeft: '20px' }}
+                    >
+                        Submit
+                    </Button>
+                </div>
+                <h2 style={{ margin: '20px' }}>{this.state.text}</h2>
+                <div style={{ color: 'red' }}>{this.state.error}</div>
+            </div>
+        );
+    }
 }
 
- 
-
 const mapStateToProps = state => {
-
     const { user, isAuthenticated, error } = state.auth;
-
     return { user, isAuthenticated, error };
-
 };
 
- 
-
-export default connect(mapStateToProps, null)(TrackerPage);
-
+export default connect(mapStateToProps, null)(ResultPage);
