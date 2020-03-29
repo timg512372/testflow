4762f9d65b7955d89d161d2b7eb89c96059dd24c
jackien1 <jackieni2002@gmail.com>
2020-03-29 15:31:13 -0700

    QR adding and saving abilities

--- berify-server/src/routes/testRouter.js
+++ berify-server/src/routes/testRouter.js
@@ -10,9 +10,8 @@ const {
   SignedTxMiddleware,
   LoomProvider
 } = require("loom-js");
-const validateRegisterInput = require("../validation/register");
-const validateLoginInput = require("../validation/login");
 const User = require("../models/user");
+const Batch = require("../models/batch");
 const TestFactory = require("../contracts/TestFactory.json");
 const Web3 = require("web3");
 const jwt = require("jsonwebtoken");
@@ -23,5 +22,78 @@ router.get("/", (req, res) => {
   res.status(200);
   res.send("Test API");
 });
+
+router.post(
+  "/new",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const user = await User.findOne({ userName: req.user.userName }).lean();
+    if (!user) {
+      res.sendStatus(400);
+    }
+
+    let QRs = [];
+
+    const privateKey = new Uint8Array(JSON.parse("[" + user.privateKey + "]"));
+    const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey);
+    const address = LocalAddress.fromPublicKey(publicKey).toString();
+
+    let client = new Client(
+      "extdev-plasma-us1",
+      "wss://extdev-plasma-us1.dappchains.com/websocket",
+      "wss://extdev-plasma-us1.dappchains.com/queryws"
+    );
+
+    client.txMiddleware = [
+      new NonceTxMiddleware(publicKey, client),
+      new SignedTxMiddleware(privateKey)
+    ];
+
+    let web3 = new Web3(new LoomProvider(client, privateKey));
+    let testFactoryInstance = new web3.eth.Contract(
+      TestFactory.abi,
+      TestFactory.networks["9545242630824"].address
+    );
+
+    for (let i = 0; i < req.body.quantity.length; i++) {
+      const id = await testFactoryInstance.methods.totalSupply().call({
+        from: address
+      });
+
+      const QR = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${user._id +
+        id}`;
+
+      await testFactoryInstance.methods.createTest(QR).send({
+        from: address
+      });
+
+      QRs.push(QR);
+    }
+
+    const newBatch = new Batch({
+      factoryId: user._id,
+      QRs,
+      quantity: req.body.quantity
+    });
+
+    await newBatch.save();
+
+    res.sendStatus(200);
+  }
+);
+
+router.get(
+  "/batches",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const user = await User.findOne({ userName: req.user.userName }).lean();
+    if (!user) {
+      res.sendStatus(400);
+    }
+
+    const batches = await Batch.find({ factoryId: user._id });
+    res.json({ batches });
+  }
+);
 
 module.exports = router;
